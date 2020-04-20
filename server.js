a4374596a5dfec321d9ad9debea1a867445f1cf3
Mrudul-Bhatt <mrudul1203bhatt@gmail.com>
2020-04-20 19:17:41 +0530

    reorganised

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+//const uri = process.env.ATLAS_URI;
 mongoose
   .connect(
     "mongodb+srv://mrudul:1810138@cluster0-hdaz0.mongodb.net/test?retryWrites=true&w=majority",
